ba8277d41cc94b06119e74e90d4262a498715ac3
Kimmo Puputti <kpuputti@gmail.com>
2013-04-08 23:37:15 +0300

    Add flymake config.

--- emacs/modules/30-flymake.el
+++ emacs/modules/30-flymake.el
@@ -1,1 +1,22 @@
 ;;; Flymake
+
+(load "flymake")
+
+;; Increase palallel checks (default 4).
+(setq flymake-max-parallel-syntax-checks 8)
+
+;; Show all errors for the line.
+(setq flymake-number-of-errors-to-display nil)
+
+;; Change backup location.
+(setq flymake-run-in-place nil)
+(setq temporary-file-directory "~/.emacs.d/tmp/")
+
+;; Enable for css-mode.
+(add-hook 'css-mode-hook
+          (lambda () (flymake-mode t)))
+
+;; Enable for sass and scss files.
+(require 'flymake-sass)
+(add-hook 'sass-mode-hook 'flymake-sass-load)
+(add-hook 'scss-mode-hook 'flymake-sass-load)
