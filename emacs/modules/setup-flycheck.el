53dd80130064b2def61db24dc424df70fbb1ce3c
Kimmo Puputti <kpuputti@gmail.com>
2013-04-23 12:54:56 +0300

    Show only fringe marker for flycheck errors.

--- emacs/modules/setup-flycheck.el
+++ emacs/modules/setup-flycheck.el
@@ -2,6 +2,8 @@
 
 (require 'flycheck)
 
+(setq flycheck-highlighting-mode 'nil)
+
 ;; Disable elisp checker.
 (eval-after-load 'flycheck
   '(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
